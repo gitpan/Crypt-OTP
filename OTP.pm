062fc9bdf2f58958d1a7c34f7ee361738fb6a9d6
Kurt Kincaid <sifukurt@yahoo.com>
2002-09-04 18:53:12 +0000

    Import of SIFUKURT/Crypt-OTP-2.00 from CPAN.
    gitpan-cpan-distribution: Crypt-OTP
    gitpan-cpan-version:      2.00
    gitpan-cpan-path:         SIFUKURT/Crypt-OTP-2.00.tar.gz
    gitpan-cpan-author:       SIFUKURT
    gitpan-cpan-maturity:     released

--- OTP.pm
+++ OTP.pm
@@ -1,37 +1,70 @@
+#-------------------------------------------------------------------------#
+# Crypt::OTP
+#       Date Written:   07-Jul-2000 02:58:42 PM
+#       Last Modified:  04-Sep-2002 01:29:36 PM
+#       Author:    Kurt Kincaid
+#       Copyright (c) 2002, Kurt Kincaid
+#           All Rights Reserved
+#
+# NOTICE: This is free software and may be modified and/or redistributed
+#         under the same terms as Perl itself.
+#-------------------------------------------------------------------------#
+
 package Crypt::OTP;
 
 use strict;
-use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+use vars qw/ $VERSION @ISA @EXPORT @EXPORT_OK $pad_text $class $pad $mode $oo /;
 
 require Exporter;
 
-@ISA = qw(Exporter AutoLoader);
+@ISA = qw/ Exporter /;
 
-$VERSION = '1.03';
-@EXPORT  = qw(OTP);
+$VERSION = '2.00';
+@EXPORT = qw/ OTP /;
 
-sub OTP {
-    my $pad_text = "";
-    my ( $pad, $message, $mode ) = @_;
-    if ($mode) {
+sub new {
+    ( $class, $pad, $mode ) = @_;
+    my $self = bless {}, $class;
+    if ( $mode ) {
         $pad_text = $pad;
     } else {
         local $/ = undef;
         open( PAD, $pad ) || return $!;
         $pad_text = <PAD>;
-        close(PAD);
+        close( PAD );
+    }
+    $oo = 1;
+    return $self;
+}
+
+sub OTP {
+    shift if UNIVERSAL::isa( $_[ 0 ], __PACKAGE__ );
+    my ( $pad, $message );
+    if ( $oo ) {
+        ( $message, $mode ) = @_;
+    } else {
+        ( $pad, $message, $mode ) = @_;
+    }
+    unless ( $oo ) {
+        if ( $mode ) {
+            $pad_text = $pad;
+        } else {
+            local $/ = undef;
+            open( PAD, $pad ) || return $!;
+            $pad_text = <PAD>;
+            close( PAD );
+        }
     }
 
-    while ( length($pad_text) < length($message) ) {
+    while ( length( $pad_text ) < length( $message ) ) {
         $pad_text .= $pad_text;
     }
     my @message = split ( //, $message );
     my @pad     = split ( //, $pad_text );
-    my $cipher  = ();
-    my $i;
+    my $cipher;
 
-    for ( $i = 0 ; $i <= $#message ; $i++ ) {
-        $cipher .= pack( 'C', unpack( 'C', $message[$i] ) ^ unpack( 'C', $pad[$i] ) );
+    for ( my $i = 0 ; $i <= $#message ; $i++ ) {
+        $cipher .= pack( 'C', unpack( 'C', $message[ $i ] ) ^ unpack( 'C', $pad[ $i ] ) );
     }
     return $cipher;
 }
@@ -46,9 +79,19 @@ Crypt::OTP - Perl implementation of the One Time Pad (hence, OTP) encryption met
 
 =head1 SYNOPSIS
 
+# OO Interface
+
+  use Crypt::OTP;
+  $ref = Crypt::OTP->new( "padfile" );
+  $cipher = $ref->OTP( $message );
+    or
+  $cipher = $ref->OTP( $message, $mode );
+  
+# Functional Interface
+
   use Crypt::OTP;
   $cipher = Crypt::OTP( $pad, $message );
-	or
+    or
   $cipher = Crypt::OTP( $pad, $message, $mode );
 
 =head1 DESCRIPTION
@@ -67,6 +110,8 @@ In this example, the "1" instructs the OTP sub-routine to use the contents of th
 
 If the file specified using the first method does not exist, OTP returns zero.  In all other cases, OTP returns the XOR'ed message.
 
+A few important points should be made about key management. First and most importantly, it should be noted that using the method where the pad is passed as a string (i.e., setting the mode to a non-zero value) is tremendously unsecure unless you use a non-repeating sequence that is at least as long as the message to be encrypted. I've had some lively debate with others on this point, but I stand firmly by the notion that key management is left as an exercise for the user. The purpose of this module is to provide One Time Pad encryption, not to provide key management for same, which is, unquestionably, a separate task. As with any encryption method, IF YOU USE IT IN AN UNSECURE FASHION, IT WILL BE UNSECURE. In any case, best practice is to use a pad that contains a pseudo-random set of data with a period greater than or equal to the length of the message to be encrypted. Why "pseudo-random"? Simple. Any random number generator (i.e., the rand() function in perl) that isn't specifically stated to be cryptographically secure, will eventually repeat its sequence of random numbers. As such, if for example your random number generator starts repeating its sequence after, say, 100 numbers, messages of less than 100 characters will be fairly secure. However, encrypted messages greater than 100 characters would be considered weak, because they would be encrypted with a pad that displays a repeating sequence. If you are uncomfortable with doing your own key management, then this is probably not the module for you. If you take proper precautions with your pad/key, Crypt::OTP will serve you in good stead. Use this module at your own risk, and use the utmost care with managing your keys.
+
 =head1 AUTHOR
 
 Kurt Kincaid, sifukurt@yahoo.com
@@ -76,3 +121,4 @@ Kurt Kincaid, sifukurt@yahoo.com
 perl(1).
 
 =cut
+
